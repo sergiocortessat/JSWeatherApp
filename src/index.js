79d1cf42686017f187c51cc343fb7a004b5a5d79
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-02 19:32:08 +0200

    Add latest bundle and separation logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import './style.css';
 import './assets/weatherFaviCon.png';
 import searchEventListener from './searchEventListener';
 
-const consoleLogData = (weatherData) => {
-  console.log(weatherData);
-};
-
 searchEventListener.EnterPress();
 searchEventListener.Click();
