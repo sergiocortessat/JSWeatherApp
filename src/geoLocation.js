ea14e7919dfbd2273bcf4d335397259e4b934cca
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-03 19:24:50 +0200

    Add build

--- src/geoLocation.js
+++ src/geoLocation.js
@@ -11,13 +11,13 @@ const errorHandler = () => {
 
 const getLocation = async (lat, lon) => {
   try {
-    const apiCall2 = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
+    const apiCall2 = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${3.4302903}&longitude=${-76.5494460}&localityLanguage=en`;
     // const apiCall = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + '54.7114102' + '%2C' + '18.4089522' + '&language=en';
     const response = await fetch(apiCall2);
     const coordinatesCity = await response.json();
     const cityName = coordinatesCity.localityInfo.administrative[3].name;
     const cityName2 = coordinatesCity.city;
-    return cityName === '' ? cityName2 : cityName;
+    return cityName2 === '' ? cityName : cityName2;
   } catch (err) {
     // status.textContent = 'Unable to find the city';
     errorHandler();
