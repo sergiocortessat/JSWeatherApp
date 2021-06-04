da297f14e53b342f9ba5b4b38ac073670c0df88b
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-04 09:28:30 +0200

    Add geolocation update

--- src/async.js
+++ src/async.js
@@ -19,6 +19,8 @@ const getWeatherData = async (location, units) => {
     content.classList.remove('d-none');
     const apiKey = '6bec4de18a94a1ec6fd65b14fff7ae04';
     const tempUnits = units;
+    // const apicall2 = `https://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=${input}`;
+    // const apicall3 = `https://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=${latitude} ,${longitude}`;
     const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${tempUnits}&appid=${apiKey}`;
     const response = await fetch(apiCall);
     const weatherData = await response.json();
