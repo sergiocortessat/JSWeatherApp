ed8bbff22e5abb2e7c2636e01d8ec9eace7b5394
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-22 11:30:57 +0200

    Add push changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import './assets/weatherFaviCon.png';
 import searchEventListener from './searchEventListener';
+import './assets/Screenshot2.png';
 
 searchEventListener.EnterPress();
 searchEventListener.Click();
